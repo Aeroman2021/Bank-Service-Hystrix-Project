394d90b4e12cb4a4e53f54888e648bf92ad0355e
malakouti.aero@gmail.com <malakouti.aero@gmail.com>
2022-12-24 23:00:43 +0330

    Bank Service created.

---
+++ src/main/java/ir/tamin/bankservice/service/AccDelegateService.java
@@ -0,0 +1,43 @@
+package ir.tamin.bankservice.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+
+@Service
+public class AccDelegateService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    @HystrixCommand(fallbackMethod = "callAccount_FallBack")
+    public String callAccService(String bankName){
+        String response = restTemplate.exchange("http://localhost:8080/api/v1/accounts/fetchAccOfTheGivenBank/{bankName}",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+                }, bankName).getBody();
+        System.out.println("response received as " + response + ": " + new Date());
+        return "NORMAL FLOW !!! - School Name -  " + bankName + " :::  included accounts " + response + " :  " + new Date();
+    }
+
+
+    @SuppressWarnings("unused")
+    private String callAccount_FallBack(String bankName){
+        System.out.println("Account Service is down!!! fallback method activated...");
+        return "Circuit Breaker Enabled" + new Date();
+
+    }
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
