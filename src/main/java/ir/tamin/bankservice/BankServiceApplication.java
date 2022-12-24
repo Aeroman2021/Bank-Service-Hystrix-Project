7c3eeb79bb1ede34cc869b615d28a39933e07f2e
malakouti.aero@gmail.com <malakouti.aero@gmail.com>
2022-12-24 22:54:16 +0330

    Initial Commit

---
+++ src/main/java/ir/tamin/bankservice/BankServiceApplication.java
@@ -0,0 +1,13 @@
+package ir.tamin.bankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankServiceApplication.class, args);
+    }
+
+}
