456bf2fc062b54fc91302fef23d72a88061880d2
malakouti.aero@gmail.com <malakouti.aero@gmail.com>
2022-12-24 22:56:12 +0330

    no message

---
+++ src/test/java/ir/tamin/bankservice/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.tamin.bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
