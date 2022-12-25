e9ab6244984ec2c28704bd483b5163875b9568c4
m_malakouti <malakouti.aero@gmail.com>
2022-12-25 11:51:50 +0330

    The commandKey = "doSomethingKey" added to CallAccService method.

--- src/main/java/ir/tamin/bankservice/service/AccDelegateService.java
+++ src/main/java/ir/tamin/bankservice/service/AccDelegateService.java
@@ -2,6 +2,7 @@ package ir.tamin.bankservice.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -11,26 +12,27 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Date;
 
 @Service
+//@EnableHystrix
 public class AccDelegateService {
 
     @Autowired
     RestTemplate restTemplate;
 
 
-    @HystrixCommand(fallbackMethod = "callAccount_FallBack")
+    @HystrixCommand(fallbackMethod = "callAccount_FallBack", commandKey = "doSomethingKey")
     public String callAccService(String bankName){
         String response = restTemplate.exchange("http://localhost:8080/api/v1/accounts/fetchAccOfTheGivenBank/{bankName}",
                 HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
                 }, bankName).getBody();
         System.out.println("response received as " + response + ": " + new Date());
-        return "NORMAL FLOW !!! - School Name -  " + bankName + " :::  included accounts " + response + " :  " + new Date();
+        return "NORMAL FLOW !!! - Bank -  " + bankName + " :::  included accounts " + response + " :  " + new Date();
     }
 
 
     @SuppressWarnings("unused")
     private String callAccount_FallBack(String bankName){
         System.out.println("Account Service is down!!! fallback method activated...");
-        return "Circuit Breaker Enabled" + new Date();
+        return "Circuit Breaker Enabled: " + new Date();
 
     }
 
