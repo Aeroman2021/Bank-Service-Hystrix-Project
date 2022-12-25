45ce177290ae9113a8ccc0ebfa0536569e8f09ce
m_malakouti <malakouti.aero@gmail.com>
2022-12-25 11:55:25 +0330

    Hystrix dashboard enabled.

--- src/main/java/ir/tamin/bankservice/BankServiceApplication.java
+++ src/main/java/ir/tamin/bankservice/BankServiceApplication.java
@@ -2,12 +2,14 @@ package ir.tamin.bankservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class BankServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BankServiceApplication.class, args);
     }
-
 }
